d2489b96d0bf599d02e356a4b4ba3b21fd1345c4
Mishieck <mishieckmwale@gmail.com>
2024-02-02 11:14:30 +0200

    feat: get all icon names

--- index.ts
+++ index.ts
@@ -3,21 +3,47 @@ import { Octokit } from 'octokit';
 const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
 const owner = 'google';
 const repo = 'material-design-icons';
-const path = '/symbols/web';
-const fullPath = '/repos/{owner}/{repo}/contents/{path}';
+const categoriesPath = '/src';
 
-const { data } = (await octokit.request(fullPath, { owner, repo, path })) as {
-  data: Array<{ type: 'file' | 'dir'; name: string }>;
+type Entry = { type: 'file' | 'dir'; name: string };
+
+type Result = {
+  data: Array<Entry>;
 };
 
-const dirNames = data
+const toNames = (entries: Array<Entry>) => entries
   .filter(({ type }) => type === 'dir')
   .map(({ name }) => name);
 
-const code = `export type MdIconName = \n| ${dirNames
+const getDirNames = async (path: string) => {
+  const result = (await octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path,
+  })) as Result;
+
+  return toNames(result.data);
+};
+
+console.info('Getting categories');
+const categories = await getDirNames(categoriesPath);
+const directories: Array<string> = [];
+
+for (const category of categories) {
+  const page = `${(categories.indexOf(category)) + 1} / ${categories.length}`;
+  console.info(`Getting icons in category '${category}' (${page})`);
+  const names = await getDirNames(`${categoriesPath}/${category}`);
+  console.info(`Got ${names.length} icons`);
+  directories.push(...names);
+  console.info('Total number of icons: ', directories.length);
+}
+
+directories.sort();
+
+const code = `export type MdIconName = \n| ${directories
   .map(name => `'${name}'`)
   .join('\n| ')};`;
 
 const filePath = 'material-icons-types.ts';
 await Bun.write(filePath, code);
-console.log(dirNames);
+
